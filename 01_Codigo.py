44c268fb422e54343ef82601a55f21a23923c2f0
NathanaKiw <nathana.silva32@gmail.com>
2023-03-16 19:45:57 -0300

    First commit

---
+++ 01_Codigo.py
@@ -0,0 +1,14 @@
+# O sistema deverá conter a leitura dos valores de parâmetros utilizados pela CETESB, para o sistema de Qualidade do ar e efeitos à saúde, que são:
+print('\n\nSistema de Controle de Qualidade do Ar e Efeitos à Saúde\n')
+
+particulasInalaveis_MP10 = int(input('\nInsira o valor das Partículas inaláveis (MP10): '))
+
+particulasInalaveisFinas_MP25 = int(input('\nInsira o valor das Partículas inaláveis finas (MP2,5): '))
+
+ozonio_o3 = int(input('\nInsira o valor do Ozônio (O³): '))
+
+monoxidoDeCarbono_co = int(input('\nInsira o valor do Monóxido de Carbono (CO): '))
+
+dioxidoDeNitrogenio_no2 = int(input('\nInsira o valor do Dióxido de Nitrogênio (NO²): '))
+
+DioxidoDeEnxofre_so2 = int(input('Insira o valor do Dióxido de Enxofre (SO²): '))
