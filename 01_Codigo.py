22923fbf85bc3db1212159c8d84049db2c0dbb04
NathanaKiw <nathana.silva32@gmail.com>
2023-03-17 21:28:05 -0300

    Adicionando condicionais

--- 01_Codigo.py
+++ 01_Codigo.py
@@ -1,14 +1,29 @@
-# O sistema deverá conter a leitura dos valores de parâmetros utilizados pela CETESB, para o sistema de Qualidade do ar e efeitos à saúde, que são:
+# O sistema deverá conter a leitura dos valores de parâmetros utilizados pela CETESB
 print('\n\nSistema de Controle de Qualidade do Ar e Efeitos à Saúde\n')
 
-particulasInalaveis_MP10 = int(input('\nInsira o valor das Partículas inaláveis (MP10): '))
+#Variável de controle de fim do programa
+continuar = 'S'
 
-particulasInalaveisFinas_MP25 = int(input('\nInsira o valor das Partículas inaláveis finas (MP2,5): '))
+while continuar == 'S' or continuar == 's':
 
-ozonio_o3 = int(input('\nInsira o valor do Ozônio (O³): '))
+    #Entradas
+    particulasInalaveis_MP10 = int(input('\nInsira o valor das Partículas inaláveis (MP10): '))
 
-monoxidoDeCarbono_co = int(input('\nInsira o valor do Monóxido de Carbono (CO): '))
+    particulasInalaveisFinas_MP25 = int(input('\nInsira o valor das Partículas inaláveis finas (MP2,5): '))
 
-dioxidoDeNitrogenio_no2 = int(input('\nInsira o valor do Dióxido de Nitrogênio (NO²): '))
+    ozonio_o3 = int(input('\nInsira o valor do Ozônio (O³): '))
 
-DioxidoDeEnxofre_so2 = int(input('Insira o valor do Dióxido de Enxofre (SO²): '))
+    monoxidoDeCarbono_co = int(input('\nInsira o valor do Monóxido de Carbono (CO): '))
+
+    dioxidoDeNitrogenio_no2 = int(input('\nInsira o valor do Dióxido de Nitrogênio (NO²): '))
+
+    DioxidoDeEnxofre_so2 = int(input('\nInsira o valor do Dióxido de Enxofre (SO²): '))
+
+    # Condicionais
+    if particulasInalaveis_MP10 < 0 or particulasInalaveisFinas_MP25 < 0 or ozonio_o3 < 0 or monoxidoDeCarbono_co < 0 or dioxidoDeNitrogenio_no2 < 0 or DioxidoDeEnxofre_so2 < 0:
+        print('\nValores menores que zero são inválidos!')
+    
+    
+
+    continuar = input('\nDeseja continuar? (S/N)    ')
+    
